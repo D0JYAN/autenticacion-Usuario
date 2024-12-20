1de30b1e7dce6a69ec0effef88854e703a20ec50
JIELSAN <jesussjg811@gmail.com>
2024-12-20 17:20:49 -0600

    implementacion de jwt

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 export const {
     PORT = 3000,
-    SALT_ROUND = 10
+    SALT_ROUND = 10,
+    SECRET_JWT_KEY = 'esta-es-la-llave-secreta-aunque-nunca-se-debe-poner-el-valor-por-defecto-en-esta-parte-para-evitar-ataques'
 } = process.env
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express'
+import jwt from 'jsonwebtoken'
 import { PORT } from './config.js'
 import { UserRepository } from './user-Repository.js'
+import { SECRET_JWT_KEY } from './config.js'
 
 const app = express()
 app.set('view engine', 'ejs')
@@ -15,6 +17,10 @@ app.post('/login', async (req, res) => {
 
     try {
         const user = await UserRepository.login({ username, password })
+        //Crear el token
+        const token = jwt.sign({ id: user._id, username: user.username }, SECRET_JWT_KEY, {
+            expiresIn: '1h'// expira en 1 hora
+        })
         res.send({ user })
     } catch (error) {
         res.status(401).send(error.message)
