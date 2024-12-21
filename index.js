96d390e569b4552812ef269f5e9d11a012d07f66
JIELSAN <jesussjg811@gmail.com>
2024-12-20 19:27:10 -0600

    middleware de validacion de sesion y cerrar sesion

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import jwt from 'jsonwebtoken'
+import cookieParser from 'cookie-parser'
 import { PORT } from './config.js'
 import { UserRepository } from './user-Repository.js'
 import { SECRET_JWT_KEY } from './config.js'
@@ -7,9 +8,23 @@ import { SECRET_JWT_KEY } from './config.js'
 const app = express()
 app.set('view engine', 'ejs')
 app.use(express.json())
+app.use(cookieParser())
+app.use((req, res, next) => {
+    const token = req.cookies.access_token
+
+    req.session = { user: null }
+
+    try {
+        const data = jwt.verify(token, SECRET_JWT_KEY)
+        req.session.user = data
+    } catch (error) {}
+
+    next()// Seguir al siguiente middleware
+})
 
 app.get('/', (req, res) => {
-    res.render('index')
+    const { user} = req.session
+    res.render('index', user)
 })
 
 app.post('/login', async (req, res) => {
@@ -21,7 +36,14 @@ app.post('/login', async (req, res) => {
         const token = jwt.sign({ id: user._id, username: user.username }, SECRET_JWT_KEY, {
             expiresIn: '1h'// expira en 1 hora
         })
-        res.send({ user })
+        res
+        .cookie('access_token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: 'strict',
+            maxAge: 60 * 60 * 1000 // expira en 1 hora
+        })
+        .send({ user, token })
     } catch (error) {
         res.status(401).send(error.message)
     }
@@ -39,13 +61,16 @@ app.post('/register', async (req, res) => {
 })
 
 app.post('/logout', (req, res) => {
-
+    res
+       .clearCookie('access_token')
+       .json({ message: 'Sesion cerrada' })
+       //Aqui tambien se puede hacer una redireccion
 })
 
 app.get('/protected', (req, res) => {
-    // TODO: if sesion del usuario
-    res.render('protected', { username: 'jesus' })
-    // TODO: else 401
+    const { user } = req.session
+    if (!user) return res.status(403).send('Acceso no autorizado')
+    res.render('protected', user)
 })
 
 app.listen(PORT, () => {
